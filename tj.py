910d9efcc737b723e86f480ed6407d9cd66d0213
Manal Jkini <143551767+mjk-22@users.noreply.github.com>
2025-10-02 17:33:53 -0400

    Update tj.py

--- tj.py
+++ tj.py
@@ -4,6 +4,8 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+#TEST IF THE WIRING WORK
+
 SuccessLED = 17
 FailLED = 27
 Buzzer = 22
@@ -12,17 +14,14 @@ GPIO.setup(SuccessLED, GPIO.OUT)
 GPIO.setup(FailLED, GPIO.OUT)
 GPIO.setup(Buzzer, GPIO.OUT)
 
-# Blue LED ON
 GPIO.output(SuccessLED, GPIO.HIGH)
 time.sleep(1)
 GPIO.output(SuccessLED, GPIO.LOW)
 
-# Red LED ON
 GPIO.output(FailLED, GPIO.HIGH)
 time.sleep(1)
 GPIO.output(FailLED, GPIO.LOW)
 
-# Buzzer ON for 1s
 GPIO.output(Buzzer, GPIO.HIGH)
 time.sleep(1)
 GPIO.output(Buzzer, GPIO.LOW)
