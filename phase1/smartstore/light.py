acc0c6606b6062b6d1799daf697d2b9cfb68c72b
Manal Jkini <143551767+mjk-22@users.noreply.github.com>
2025-10-02 19:55:23 -0400

    deleted the old commented code in light.py

--- phase1/smartstore/light.py
+++ phase1/smartstore/light.py
@@ -1,5 +1,3 @@
-# import RPi.GPIO as GPIO
-# from time import sleep   
 import os
 import time
 
@@ -58,33 +56,3 @@ def clear_output():
     GPIO.output(FailLED,GPIO.LOW)
     GPIO.output(Buzzer,GPIO.LOW)
     GPIO.output(SuccessLED,GPIO.LOW)
-# GPIO.cleanup()
-# class Result:
-#     def output(self, desired_output:bool):
-#             GPIO.setmode(GPIO.BCM)
-#             GPIO.setwarnings(False)
-#             SuccessLED = 17
-#             FailLED = 27
-#             Buzzer = 22
-            
-            
-              
-             
-#             if desired_output:
-#                 GPIO.setup(SuccessLED,GPIO.OUT)
-#                 GPIO.output(SuccessLED,GPIO.HIGH)
-#                 sleep(3)
-#             else:
-#                 GPIO.setup(FailLED,GPIO.OUT)
-#                 GPIO.output(FailLED,GPIO.HIGH)
-#                 GPIO.setup(Buzzer,GPIO.OUT)
-#                 p = GPIO.PWM(Buzzer, 1000)
-#                 GPIO.output(Buzzer,GPIO.HIGH)
-#                 p.start(50)
-#                 sleep(3)
-#             GPIO.cleanup()
-        
-# r = Result()
-# r.output(True)
-# GPIO.cleanup()
-
