b2fccedeccb59dbe27df5f4cfe32e613650e11ea
Trevor <maclellan.trevor@gmail.com>
2025-10-22 20:05:08 -0400

    added directory for phase 2

--- tj.py
+++ tj.py
@@ -6,24 +6,24 @@ GPIO.setwarnings(False)
 
 #TEST IF THE WIRING WORK
 
-SuccessLED = 17
+# SuccessLED = 17
 FailLED = 27
-Buzzer = 22
+# Buzzer = 22
 
-GPIO.setup(SuccessLED, GPIO.OUT)
+# GPIO.setup(SuccessLED, GPIO.OUT)
 GPIO.setup(FailLED, GPIO.OUT)
-GPIO.setup(Buzzer, GPIO.OUT)
+# GPIO.setup(Buzzer, GPIO.OUT)
 
-GPIO.output(SuccessLED, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(SuccessLED, GPIO.LOW)
+# GPIO.output(SuccessLED, GPIO.HIGH)
+# time.sleep(1)
+# GPIO.output(SuccessLED, GPIO.LOW)
 
 GPIO.output(FailLED, GPIO.HIGH)
 time.sleep(1)
 GPIO.output(FailLED, GPIO.LOW)
 
-GPIO.output(Buzzer, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(Buzzer, GPIO.LOW)
+# GPIO.output(Buzzer, GPIO.HIGH)
+# time.sleep(1)
+# GPIO.output(Buzzer, GPIO.LOW)
 
 GPIO.cleanup()
