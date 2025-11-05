a068663065c4ae2957ef3351ed042f66b82c2856
Trevor <maclellan.trevor@gmail.com>
2025-11-05 10:18:42 -0500

    Replaced phone field for regex field

--- phase3/phase3/models.py
+++ phase3/phase3/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.core.validators import *
-from phone_field import *
+from django import forms
 
-class Customers(models.Model):
+class Customers(models.Model, forms.ModelForm):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100, unique=True)
-    phone_number = PhoneFormField()
+    phone_number = forms.RegexField(regex=r'^\d{9,15}$')
     password = models.CharField(null=False)
     membership_id = models.CharField(max_length=255, unique=True)
     points = models.PositiveIntegerField(default=0, null=False) 
